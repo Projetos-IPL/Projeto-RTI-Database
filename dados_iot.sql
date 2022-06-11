c82623ed63b0bd39bfe3ff38a9f51d0fa1f85a4e
Afonso Santos <afonso.faisca1@gmail.com>
2022-06-11 21:46:27 +0100

    Update dados_iot.sql

--- dados_iot.sql
+++ dados_iot.sql
@@ -1,7 +1,7 @@
-INSERT INTO sensor (sensor_id, name) VALUES (1, 'Sensor de temperatura');
-INSERT INTO sensor (sensor_id, name) VALUES (2, 'Sensor de humidade');
-INSERT INTO sensor (sensor_id, name) VALUES (3, 'Sensor de fumo');
-INSERT INTO sensor (sensor_id, name) VALUES (4, 'Sensor de movimento');
+INSERT INTO sensor (sensor_id, name) VALUES (1, 'Sensor de Temperatura');
+INSERT INTO sensor (sensor_id, name) VALUES (2, 'Sensor de Humidade');
+INSERT INTO sensor (sensor_id, name) VALUES (3, 'Sensor de Fumo');
+INSERT INTO sensor (sensor_id, name) VALUES (4, 'Sensor de Movimento');
 INSERT INTO sensor (sensor_id, name) VALUES (5, 'Leitor RFID');
 
 INSERT INTO actuator (actuator_id, name) VALUES (1, 'Porta (Frente)');
